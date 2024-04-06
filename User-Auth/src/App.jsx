bc65c6532ecf32ae0beae3f7107113e7160a4bc1
Yasir Ali <91179875+yasirali786110@users.noreply.github.com>
2024-04-06 18:16:55 +0500

    setup of mern stack app

---
+++ User-Auth/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      <h1 className=' text-center text-indigo-800 text-4xl'>User Authentication</h1>
+
+    </>
+  )
+}
+
+export default App
