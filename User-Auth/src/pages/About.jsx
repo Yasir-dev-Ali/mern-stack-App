67f4367b45b2f1b510a9f8832a867234eaa84754
Yasir Ali <91179875+yasirali786110@users.noreply.github.com>
2024-04-20 15:40:02 +0500

    Beck End Create

--- User-Auth/src/pages/About.jsx
+++ User-Auth/src/pages/About.jsx
@@ -7,8 +7,7 @@ const About = () => {
     <div className="flex justify-center items-center h-screen bg-gray-200">
       <div className="p-10 bg-white rounded shadow-lg">
         <h1 className="text-2xl font-bold mb-10">About Us</h1>
-        <p className="text-gray-600">Lorem ipsum dolor sit amet consectetur adipisicing elit
-          . Quisquam, voluptate! Quisquam, voluptate!</p>
+        <p className="mb-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur</p>
         <img src="https://source.unsplash.com/random/400x200" alt="about" className="mt-5" />
       </div>
     </div>
