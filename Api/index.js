5c2ebc7de7ea76cb582f44ea8534928558e96f8b
Yasir Ali <91179875+yasirali786110@users.noreply.github.com>
2024-04-21 00:57:52 +0500

    connecte the database

--- Api/index.js
+++ Api/index.js
@@ -1,4 +1,19 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+mongoose.connect(process.env.mongo).then(() => {
+    console.log('Connected to MongoDB ! ');
+}).catch((err) => {
+    console.log('Error : ', err);
+}
+);
+
+
+
+
+
+
 
 const app = express();
 
