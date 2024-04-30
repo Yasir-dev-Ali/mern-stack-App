87e01ce8a3d0958cd4bad009439fdebf2e6f7feb
Yasir Ali <91179875+yasirali786110@users.noreply.github.com>
2024-04-30 20:34:31 +0500

    SginUp page complete it with the functionality

--- User-Auth/src/pages/SignUp.jsx
+++ User-Auth/src/pages/SignUp.jsx
@@ -1,61 +1,68 @@
-import React from 'react';  
+import React, { useState } from 'react';  
 import { Link } from 'react-router-dom';
 
 const SignUp = () => {
+    const [formdata, setFormdata] = useState({});
+    const [loading, setloading] = useState(false);
+
+   
+
+ 
+
+    const handleChange = (e) => {
+        setFormdata({ ...formdata, [e.target.name]: e.target.value });
+
+    };
+   const handleSubmit= async (e)=>{
+    e.preventDefault();
+    try {
+        setloading(true);
+         
+        const response = await fetch('/api/auth/signup',{
+            method:'POST',
+            headers:{
+                'Content-Type':'application/json'
+            },
+            body: JSON.stringify(formdata)
+              
+        } );
+        const data= await response.json();
+        setloading(false);
+        if(data.error){
+            alert(data.error);
+        }
+         
+    
+
+    } catch (error) {
+        setloading(false);
+        
+    }
+
+
+   }
     return (
         <div className="flex justify-center items-center h-screen">
             <div className="w-1/3">
                 <h1 className="text-center text-indigo-800 text-4xl">Sign Up</h1>
-                <form className="mt-10">
-                    <div className="mb-6">
-                        <label
-                            htmlFor="name"
-                            className="block text-indigo-800 text-sm font-medium mb-2"
-                        >
-                            Name
-                        </label>
-                        <input
-                            type="text"
-                            id="name"
-                            name="name"
-                            placeholder="Your Name"
-                            className="w-full p-2 border border-indigo-800"
-                        />
-                    </div>
-
+                <form className="mt-10" onSubmit={handleSubmit}>
 
                     <div className="mb-6">
-                        <label
-                            htmlFor="email"
-                            className="block text-indigo-800 text-sm font-medium mb-2"
-                        >
-                            Email
-                        </label>
-                        <input
-                            type="email"
-                            id="email"
-                            name="email"
-                            placeholder="Your Email"
-                            className="w-full p-2 border border-indigo-800"
-                        />
+                        <label htmlFor="name" className="block text-indigo-800 text-sm font-medium mb-2">Name </label>
+                        <input type="text" name="name" id="name" placeholder="Your Name" onChange={handleChange} className="w-full p-3 rounded border border-gray-300" />
                     </div>
                     <div className="mb-6">
-                        <label
-                            htmlFor="password"
-                            className="block text-indigo-800 text-sm font-medium mb-2"
-                        >
-                            Password
-                        </label>
-                        <input
-                            type="password"
-                            id="password"
-                            name="password"
-                            placeholder="Your Password"
-                            className="w-full p-2 border border-indigo-800"
-                        />
+                        <label htmlFor="email" className="block text-indigo-800 text-sm font-medium mb-2">Email</label>
+                        <input type="email" name="email" id="email" placeholder="Your Email" onChange={handleChange} className="w-full p-3 rounded border border-gray-300" />
                     </div>
+                    <div className="mb-6">
+                        <label htmlFor="password" className="block text-indigo-800 text-sm font-medium mb-2">Password</label>
+                        <input type="password" name="password" id="password" placeholder="Your Password" onChange={handleChange} className="w-full p-3 rounded border border-gray-300" />
+                    </div>
+
                     <button className="w-full bg-indigo-800 hover:bg-indigo-600 text-white font-semibold p-3">
-                        Sign Up
+                      {loading ? 'Loading...': 'Sign Up'}
+
                     </button>
                     <div className="text-center my-4">Or</div>
                     < button className = "w-full bg-red-800 hover:bg-red-600 text-white font-semibold p-3" >
